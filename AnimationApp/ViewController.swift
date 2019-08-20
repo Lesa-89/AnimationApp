fee9f88d0319835a54b578c20e97896f79297079
Larichev Sergey <lesa-89@mail.ru>
2019-08-20 21:57:14 +0300

    Initial Commit

---
+++ AnimationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationApp
+//
+//  Created by macbook on 20/08/2019.
+//  Copyright © 2019 macbook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
