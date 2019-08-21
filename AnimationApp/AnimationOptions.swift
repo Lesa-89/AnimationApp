97c1799b84c7d6d4ca2073995c3c1bcefe0c51f6
Larichev Sergey <lesa-89@mail.ru>
2019-08-21 09:36:04 +0300

    New

---
+++ AnimationApp/AnimationOptions.swift
@@ -0,0 +1,69 @@
+//
+//  AnimationOptions.swift
+//  
+//
+//  Created by macbook on 20/08/2019.
+//
+
+import Spring
+
+let animations: [Spring.AnimationPreset] = [
+    .Shake,
+    .Pop,
+    .Morph,
+    .Squeeze,
+    .Wobble,
+    .Swing,
+    .FlipX,
+    .FlipY,
+    .Fall,
+    .SqueezeLeft,
+    .SqueezeRight,
+    .SqueezeDown,
+    .SqueezeUp,
+    .SlideLeft,
+    .SlideRight,
+    .SlideDown,
+    .SlideUp,
+    .FadeIn,
+    .FadeOut,
+    .FadeInLeft,
+    .FadeInRight,
+    .FadeInDown,
+    .FadeInUp,
+    .ZoomIn,
+    .ZoomOut,
+    .Flash
+]
+
+var animationCurves: [Spring.AnimationCurve] = [
+    .EaseIn,
+    .EaseOut,
+    .EaseInOut,
+    .Linear,
+    .Spring,
+    .EaseInSine,
+    .EaseOutSine,
+    .EaseInOutSine,
+    .EaseInQuad,
+    .EaseOutQuad,
+    .EaseInOutQuad,
+    .EaseInCubic,
+    .EaseOutCubic,
+    .EaseInOutCubic,
+    .EaseInQuart,
+    .EaseOutQuart,
+    .EaseInOutQuart,
+    .EaseInQuint,
+    .EaseOutQuint,
+    .EaseInOutQuint,
+    .EaseInExpo,
+    .EaseOutExpo,
+    .EaseInOutExpo,
+    .EaseInCirc,
+    .EaseOutCirc,
+    .EaseInOutCirc,
+    .EaseInBack,
+    .EaseOutBack,
+    .EaseInOutBack
+]
--- AnimationApp/ViewController.swift
+++ AnimationApp/ViewController.swift
@@ -6,15 +6,47 @@
 //  Copyright © 2019 macbook. All rights reserved.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var springView: SpringView!
+    @IBOutlet var springButton: SpringButton!
+    @IBOutlet var springLabel: SpringLabel!
+    
+    
+    let cornerRadius: CGFloat = 20
+    var animation = animations.randomElement()!.rawValue
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        springButton.layer.cornerRadius = cornerRadius
+        springView.layer.cornerRadius = cornerRadius
+        
+        springView.animate()
+        springButton.animate()
     }
-
-
+    
+    @IBAction func springButtonPressed(_ sender: SpringButton) {
+        springView.animation = animation
+        springView.curve = animationCurves.randomElement()!.rawValue
+        springView.force = CGFloat.random(in: 1...5)
+        springView.duration = CGFloat.random(in: 0.5...5)
+        springView.delay = CGFloat.random(in: 0...1)
+        springView.rotate = CGFloat.random(in: 0...5)
+        springView.damping = CGFloat.random(in: 0...1)
+        
+        springLabel.text = "animation: \(animation)\n curve: \(springView.curve) \n force: = \(NSString(format: "%.1f", springView.force)) \n duration: \(NSString(format: "%.1f", springView.duration)) \n delay: \(NSString(format: "%.1f", springView.delay)) \n rotate: \(NSString(format: "%.1f", springView.rotate)) \n damping: \(NSString(format: "%.1f", springView.damping))"
+        
+        springView.animate()
+        
+        animation = animations.randomElement()!.rawValue
+        springButton.setTitle(animation, for: .normal)
+        springButton.animation = "pop"
+        springButton.curve = "easelnOut"
+        springButton.animate()
+    }
+    
 }
 
